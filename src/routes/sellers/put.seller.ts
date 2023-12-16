52e35063523737f8905db8fe714d74186e6a4e35
dipakmahajan <priyanka.raval6@gmail.com>
2023-12-16 17:20:43 +0530

    changes password api changes

--- src/routes/sellers/put.seller.ts
+++ src/routes/sellers/put.seller.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import User from '../../model/user.model';
-import { comparePassword, hashPassword } from '../../common/common-function';
+import { comparePassword, getUserData, hashPassword } from '../../common/common-function';
 import { ERROR, USER } from '../../common/global-constants';
 import { responseGenerators } from '../../lib';
 
 export const changePasswordHandler = async (req: Request, res: Response) => {
   try {
     const { old_password: oldPassword, new_password: newPassword } = req.body;
-    const { tokenData } = req.headers;
-    const userData = await User.findOne({ user_id: tokenData.user_id, is_deleted: false });
+    const { user_id: userId } = getUserData(req);
+    const userData = await User.findOne({ user_id: userId, is_deleted: false });
     if (!userData) {
       return res.status(StatusCodes.BAD_REQUEST).send(responseGenerators({}, StatusCodes.BAD_REQUEST));
     }
@@ -18,7 +18,7 @@ export const changePasswordHandler = async (req: Request, res: Response) => {
       return res.status(StatusCodes.BAD_REQUEST).send(responseGenerators({}, StatusCodes.BAD_REQUEST));
     }
     const bcryptPassword = await hashPassword(newPassword);
-    await User.updateOne({ user_id: tokenData.user_id, is_deleted: false }, { password: bcryptPassword });
+    await User.updateOne({ user_id: userId, is_deleted: false }, { password: bcryptPassword });
     return res.status(StatusCodes.OK).send(responseGenerators({}, StatusCodes.OK, USER.PASSWORD_UPDATE));
   } catch (error) {
     return res
