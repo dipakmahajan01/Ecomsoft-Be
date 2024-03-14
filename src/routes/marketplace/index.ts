b7084e4198e17989936de804171966e932dcedf4
dipak <dipakmahajan2808200>
2024-03-14 22:41:05 +0530

    authenication middleware added in marketPlace index file

--- src/routes/marketplace/index.ts
+++ src/routes/marketplace/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { addMarketPlaceCred } from './post.credential';
 import { getMarketplaceCred } from './get.credentials';
+import { authentication } from '../../middleware/authenticate-user';
 
 const marketPlaceRoutes = Router();
 
-// marketPlaceRoutes.use(authentication);
+marketPlaceRoutes.use(authentication);
 marketPlaceRoutes.post('/add', addMarketPlaceCred);
 marketPlaceRoutes.get('/', getMarketplaceCred);
 
