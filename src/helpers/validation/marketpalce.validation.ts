20307e181d0761f9a01d9ec76df0ba301741704c
dipak <dipakmahajan2808200>
2024-06-04 22:41:48 +0530

    market place added api changes

--- src/helpers/validation/marketpalce.validation.ts
+++ src/helpers/validation/marketpalce.validation.ts
@@ -3,6 +3,4 @@ import Joi from 'joi';
 export const addMarketPlaceSchemaHandler = Joi.object({
   market_place_name: Joi.string().optional(),
   account_name: Joi.string().required(),
-  badge: Joi.string().optional(),
-  code: Joi.string().required(),
 });
