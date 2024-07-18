42448b93d16d371d715854777107e3076892d09a
dipak <dipakmahajan2808200>
2024-07-18 23:59:43 +0530

    added changes for update api

--- src/routes/sheet_order/put.sheeOrder.ts
+++ src/routes/sheet_order/put.sheeOrder.ts
@@ -2,13 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import { responseGenerators } from '../../lib';
 import { ERROR, ORDER } from '../../common/global-constants';
-import PaymentOrders from '../../model/payment_order.model';
 import Order from '../../model/sheet_order.model';
 
 export const updateReturnOrderHandler = async (req: Request, res: Response) => {
   try {
     const { order_id: orderId } = req.query;
-    const orderFound = await PaymentOrders.findOne({ subOrderNo: orderId });
+    const orderFound = await Order.findOne({ sub_order_no: orderId });
     if (!orderFound) {
       return res.status(StatusCodes.OK).send(responseGenerators({}, StatusCodes.OK, ORDER.NOT_FOUND, false));
     }
