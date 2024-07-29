c52d49b2c5665edb84b02bfba7b333ef3a47c12d
dipak <dipakmahajan2808200>
2024-07-29 23:36:21 +0530

    fix:middleware uncomment

--- src/routes/sheet_order/index.ts
+++ src/routes/sheet_order/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
-// import { authentication } from '../../middleware/authenticate-user';
+import { authentication } from '../../middleware/authenticate-user';
 import { paymentOrderUpload, uploadOrderSheetHandler } from './post.order';
 
 import { getAnalyticsHandler, getSellerAnalyticsHandler, returnOrderHandler } from './get.sheetOrder';
 import { updateReturnOrderHandler } from './put.sheeOrder';
 
 const sheetOrder = Router();
-// sheetOrder.use(authentication);
+sheetOrder.use(authentication);
 const storage = multer.memoryStorage();
 const upload = multer({ storage, dest: '/uploads' });
 sheetOrder.post('/upload', upload.single('order_sheet'), uploadOrderSheetHandler);
