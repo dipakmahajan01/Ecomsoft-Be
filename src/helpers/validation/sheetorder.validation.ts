6621cb8a2aa01b430fcda9c191370355b0c20d4f
dipak <dipakmahajan2808200>
2024-07-30 01:03:37 +0530

    fix:validation issue

--- src/helpers/validation/sheetorder.validation.ts
+++ src/helpers/validation/sheetorder.validation.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const returnOrderSchema = Joi.object({
   account_id: Joi.string().allow('').optional(),
-  is_return_update: Joi.string().allow().required(),
-  status: Joi.string().allow().optional(),
-  is_order_issue: Joi.string().allow().optional(),
+  is_return_update: Joi.string().allow('').optional(),
+  status: Joi.string().allow('').optional(),
+  is_order_issue: Joi.string().allow('').optional(),
 });
