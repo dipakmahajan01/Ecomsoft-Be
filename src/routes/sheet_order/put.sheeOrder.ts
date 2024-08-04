3bc846ca9cf3f45befd90f33adca65d860543095
dipak <dipakmahajan2808200>
2024-08-04 23:14:37 +0530

    fix :: update return related changes added

--- src/routes/sheet_order/put.sheeOrder.ts
+++ src/routes/sheet_order/put.sheeOrder.ts
@@ -4,11 +4,21 @@ import { generatePublicId } from '../../common/common-function';
 import { responseGenerators } from '../../lib';
 import { ERROR, ORDER } from '../../common/global-constants';
 import OrderTracking from '../../model/order_tracking.mode';
+import Order from '../../model/sheet_order.model';
 
 export const updateReturnOrderHandler = async (req: Request, res: Response) => {
   try {
     // await updateReturnOrderSchema.validateAsync(req.query);
     const { account_id: accountId, order_id: OrderId } = req.body;
+    const orderFound = await Order.findOne({ awb_number: OrderId });
+    if (!orderFound) {
+      return res.status(StatusCodes.OK).send(responseGenerators({}, StatusCodes.OK, ORDER.NOT_FOUND, false));
+    }
+    const updateOrder = await Order.findOneAndUpdate(
+      { awb_number: OrderId },
+      { $set: { is_return_update: true } },
+      { returnOriginal: false },
+    );
     const orderTrackingData = await OrderTracking.findOne({ account_id: accountId });
     if (orderTrackingData) {
       await OrderTracking.findOneAndUpdate(
@@ -27,7 +37,10 @@ export const updateReturnOrderHandler = async (req: Request, res: Response) => {
         return res.status(StatusCodes.OK).send(responseGenerators(createOrder, StatusCodes.OK, ORDER.UPDATE, false));
       }
     }
-
+    if (!updateOrder) {
+      return res.status(StatusCodes.OK).send(responseGenerators({}, StatusCodes.OK, ORDER.NOT_UPDATE, false));
+    }
+    return res.status(StatusCodes.OK).send(responseGenerators(updateOrder, StatusCodes.OK, ORDER.UPDATE, false));
     // const orderFound = await Order.findOne({ sub_order_no: orderId });
     // if (!orderFound) {
     //   return res
@@ -50,7 +63,6 @@ export const updateReturnOrderHandler = async (req: Request, res: Response) => {
     //     .status(StatusCodes.BAD_REQUEST)
     //     .send(responseGenerators({}, StatusCodes.BAD_REQUEST, ORDER.NOT_UPDATE, false));
     // }
-    return res.status(StatusCodes.OK).send(responseGenerators({}, StatusCodes.OK, ORDER.UPDATE, false));
   } catch (error) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
